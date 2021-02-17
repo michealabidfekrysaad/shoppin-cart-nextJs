1d05b860792e920f07f67605982fb8375a14eb4d
michealabidfekrysaad <michealabidfekry@gmail.com>
2021-02-17 15:05:35 +0200

    add error page, home-page, some-css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../component/Layout";
+import { Provider } from "react-redux";
+import store from "../store/Index";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "../styles/Home.module.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // <Provider store={store}>
+      // <Router>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      //  </Router>
+    // </Provider>
+    // <h1>index</h1>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,143 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux"; 
+import { ProductsRequest } from "../store/actions/Products";
+import { AddToCart } from "../store/actions/Cart";
+import { useHistory } from "react-router-dom";
+
+import Product from "../component/Product/Product";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Btn from "../component/Btn/Btn";
+import Pagination from "../component/Pagination/Pagination";
+import Loader from "../component/Loader/Loader";
+import { useRouter, Router } from "next/router";
+
+
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//     paddingTop: 20,
+//   },
+//   paper: {
+//     padding: theme.spacing(1),
+//     textAlign: "center",
+//     color: theme.palette.text.secondary,
+//   },
+//   rootCard: {
+//     maxWidth: 345,
+//   },
+//   media: {
+//     height: 140,
+//   },
+// }));
 
 export default function Home() {
+  const products = useSelector((state) => state.ProductsReducer);
+  const loading = useSelector((state) => state.loader);
+  const cartProducts = useSelector((state) => state.CartReducer);
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [ProductsPerPage] = useState(4);
+  const indexOfLastProduct = currentPage * ProductsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - ProductsPerPage;
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // const classes = useStyles();
+  // const history = useHistory();
+  const router = useRouter();
+
+
+  useEffect(() => {
+    dispatch(ProductsRequest());
+  }, [dispatch]);
+
+  let currentProducts = 0;
+  products.length &&
+    (currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct));
+
+  const addProdToCart = (product) => {
+    let existed_item = cartProducts.products.find(
+      (singleProduct) => singleProduct.id === product.id
+    );
+    if (existed_item) {
+      existed_item.qty += 1;
+      existed_item.total += existed_item.price;
+      // AddToCart("");
+      dispatch(AddToCart(""));
+    } else {
+      product.qty = 1;
+      product.total = product.price;
+      // AddToCart(product);
+      dispatch(AddToCart(product));
+    }
+      router.push("/cart");
+  };
+
+  const goToProductDetails = (prod) => {
+    router.push({
+      pathname: `/product-details/${prod.id}`,
+      state: `${prod.id}`,
+    });
+  };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className={styles.root}>
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          {currentProducts.length
+            ? currentProducts.map(
+                ({ id, title, description, image, price }) => {
+                  return (
+                    <Grid item xs={3} key={id}>
+                      <Paper className={styles.paper}>
+                        <Product
+                          cardInfo={{
+                            id,
+                            image,
+                            title,
+                            description,
+                            price,
+                            addProdToCart,
+                          }}
+                        />
+                        <hr />
+                        <Btn
+                          size="small"
+                          variant="outlined"
+                          color="secondary"
+                          content="Details"
+                          handleClick={() =>
+                            goToProductDetails({
+                              id,
+                              title,
+                              description,
+                              image,
+                              price,
+                            })
+                          }
+                        />
+                      </Paper>
+                    </Grid>
+                  );
+                }
+              )
+            : loading && <Loader />}
+          {products.length && (
+            <div className="container-fluid-pagination">
+              <Pagination
+                productsPerPage={4}
+                totalProducts={products.length}
+                paginate={paginate}
+              />
+            </div>
+          )}
+        </Grid>
+      </Grid>
     </div>
   )
 }
